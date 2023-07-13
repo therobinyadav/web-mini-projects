8df264bb17a0d14ac8ed644f519d939e5d75e1aa
Robin Yadav <100231104+therobinyadav@users.noreply.github.com>
2023-07-13 18:20:57 +0530

    Update: Set min/max, check input values and float result
    Write IIFE to set min/max attributes for inputs. Write conditional statements to check whether input values are valid and change them accordingly if they aren't. Display the result as decimal value.

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -5,8 +5,25 @@ const billInputEl = document.querySelector('.bill-input');
 const tipInputEl = document.querySelector('.tip-input');
 const resultEL = document.querySelector('.result');
 
-btnEl.addEventListener('click', function () {
-  resultEL.textContent = `${
+// Initialization
+(function () {
+  billInputEl.setAttribute('min', 0);
+  tipInputEl.setAttribute('min', 0);
+  tipInputEl.setAttribute('max', 100);
+})();
+
+btnEl.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (billInputEl.value < 0) {
+    billInputEl.value = 0;
+  } else if (tipInputEl.value < 0) {
+    tipInputEl.value = 0;
+  } else if (tipInputEl.value > 100) {
+    tipInputEl.value = 100;
+  }
+
+  resultEL.textContent = Number.parseFloat(
     +billInputEl.value + +billInputEl.value * (+tipInputEl.value / 100)
-  }`;
+  ).toFixed(2);
 });
