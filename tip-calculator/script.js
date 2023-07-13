19a4f4090ec9e0bd95389f6c95c194e1a8f69152
Robin Yadav <100231104+therobinyadav@users.noreply.github.com>
2023-07-13 11:53:19 +0530

    Update: Write calculation logic
    Write logic to calculate total amount using bill and tip when button is clicked.

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const btnEl = document.querySelector('.btn');
+const billInputEl = document.querySelector('.bill-input');
+const tipInputEl = document.querySelector('.tip-input');
+const resultEL = document.querySelector('.result');
+
+btnEl.addEventListener('click', function () {
+  resultEL.textContent = `${
+    +billInputEl.value + +billInputEl.value * (+tipInputEl.value / 100)
+  }`;
+});
