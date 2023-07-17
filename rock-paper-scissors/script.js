43b4d93878235f5649d175b46850ac51d4e582e4
Robin Yadav <100231104+therobinyadav@users.noreply.github.com>
2023-07-17 14:16:18 +0530

    Update: Make what you and computer chose more easily visible
    Give a different background color when you choose an option and create computer options with same look as buttons and different background color to the option that computer chose.

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const buttons = document.querySelectorAll('.btn');
 const btnRockEl = document.querySelector('.btn-rock');
 const btnPaperEl = document.querySelector('.btn-paper');
 const btnScissorEl = document.querySelector('.btn-scissor');
-const computerChoiceEl = document.querySelector('.computer-choice');
+const computerMoveEl = document.querySelector('.computer-move');
+const computerOptions = document.querySelectorAll('.computer-option');
 const resultEl = document.querySelector('.result');
 
 let yourChoice;
@@ -14,8 +16,8 @@ const computerTurn = function () {
   if (randomNum === 1) computerChoice = '✊';
   if (randomNum === 2) computerChoice = '✋';
   if (randomNum === 3) computerChoice = '✌️';
-
-  computerChoiceEl.textContent = `Computer chose: ${computerChoice}`;
+  removeChosenClass(computerOptions);
+  computerOptions[randomNum - 1].classList.add('chosen');
 };
 
 const checkWinner = function () {
@@ -44,20 +46,30 @@ const checkWinner = function () {
   resultEl.textContent = resultText;
 };
 
+const removeChosenClass = function (nodeList) {
+  nodeList.forEach(el => el.classList.remove('chosen'));
+};
+
 btnRockEl.addEventListener('click', function () {
   yourChoice = btnRockEl.textContent;
+  removeChosenClass(buttons);
+  btnRockEl.classList.add('chosen');
   computerTurn();
   checkWinner();
 });
 
 btnPaperEl.addEventListener('click', function () {
   yourChoice = btnPaperEl.textContent;
+  removeChosenClass(buttons);
+  btnPaperEl.classList.add('chosen');
   computerTurn();
   checkWinner();
 });
 
 btnScissorEl.addEventListener('click', function () {
   yourChoice = btnScissorEl.textContent;
+  removeChosenClass(buttons);
+  btnScissorEl.classList.add('chosen');
   computerTurn();
   checkWinner();
 });
