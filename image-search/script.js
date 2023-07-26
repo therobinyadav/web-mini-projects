736ed6c01a64b55fa7abaa2b07ebcf4280d25d3d
Robin Yadav <100231104+therobinyadav@users.noreply.github.com>
2023-07-26 18:37:36 +0530

    Update: Handle errors
    Use try and catch to handle errors when making HTTP requests. Display a message in place of image results if any error has occurred.

--- image-search/script.js
+++ image-search/script.js
@@ -7,17 +7,22 @@ const searchInputEl = document.querySelector('.search-input');
 const btnSearchEl = document.querySelector('.btn-search');
 
 const getImages = async function (ACCESS_KEY, query) {
-  const res = await fetch(
-    `https://api.unsplash.com/search/photos/?per_page=30&client_id=${ACCESS_KEY}&query=${query}`
-  );
-  const { results } = await res.json();
-  return results.map(result => {
-    return {
-      imgUrl: result.urls.regular,
-      alt: result.alt_description,
-      link: result.links.html,
-    };
-  });
+  try {
+    const res = await fetch(
+      `https://api.unsplash.com/search/photos/?per_page=30&client_id=${ACCESS_KEY}&query=${query}`
+    );
+    if (!res.ok) throw new Error();
+    const { results } = await res.json();
+    return results.map(result => {
+      return {
+        imgUrl: result.urls.regular,
+        alt: result.alt_description,
+        link: result.links.html,
+      };
+    });
+  } catch {
+    imageResultsEL.textContent = 'Something went wrong :(';
+  }
 };
 
 const renderImages = function (imgArr) {
@@ -27,9 +32,9 @@ const renderImages = function (imgArr) {
     imgArr.forEach(img => {
       markup += `
       <div class="image-result">
-      <a href="${img.imgUrl}" target="_blank">
-      <img class="image" src="${img.imgUrl}" alt="${img.alt}" />
-      </a>
+        <a href="${img.imgUrl}" target="_blank">
+          <img class="image" src="${img.imgUrl}" alt="${img.alt}" />
+        </a>
       </div>
       `;
     });
