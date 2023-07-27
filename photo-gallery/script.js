99b18fea2ee4415f7131bb2d94b98ba9ff868e07
Robin Yadav <100231104+therobinyadav@users.noreply.github.com>
2023-07-27 17:07:56 +0530

    Update: Fetch and display images
    Fetch images and display them according to the chosen grid.

--- photo-gallery/script.js
+++ photo-gallery/script.js
@@ -3,17 +3,68 @@
 const gridOptionsEl = document.querySelector('.grid-options');
 const galleryEl = document.querySelector('.gallery');
 
+const KEY = '1PxpVrDBqXwLB8mG9JkXiufKelmopLOgJc48i0oE98g';
+let images = [];
 let gridClass = `grid-${gridOptionsEl.value}`;
+let totalImages = (+gridOptionsEl.value) ** 2;
 
 // Default grid
 galleryEl.classList.add(gridClass);
 
+const getImages = async function (ACCESS_KEY) {
+  try {
+    const res = await fetch(
+      `https://api.unsplash.com/photos/random/?count=25&client_id=${ACCESS_KEY}`
+    );
+    if (!res.ok) {
+      if (res.status === 403) {
+        throw new Error('Rate limit exceeded! Try again later.');
+      } else {
+        throw new Error('Something went wrong :(');
+      }
+    }
+    let results = await res.json();
+    images = results.map(result => {
+      return {
+        imgUrl: result.urls.regular,
+        alt: result.alt_description,
+        link: result.links.html,
+      };
+    });
+    if (images.length) {
+      renderImages(images);
+    } else {
+      throw new Error('No images at this moment :(');
+    }
+  } catch (err) {
+    galleryEl.textContent = err.message;
+  }
+};
+
+// This needs to be called only once
+getImages(KEY);
+
+const renderImages = function (imgArr) {
+  galleryEl.innerHTML = '';
+  let markup = '';
+  imgArr.slice(0, totalImages).forEach(img => {
+    markup += `
+      <div class="image-box">
+        <img class="image" src="${img.imgUrl}" alt="${img.alt}" />
+      </div>
+    `;
+  });
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+};
+
 const updateGrid = function () {
   gridClass = `grid-${gridOptionsEl.value}`;
+  totalImages = (+gridOptionsEl.value) ** 2;
   for (let i = 2; i <= 5; i++) {
     galleryEl.classList.remove(`grid-${i}`);
   }
   galleryEl.classList.add(gridClass);
+  renderImages(images);
 };
 
 gridOptionsEl.addEventListener('input', updateGrid);
