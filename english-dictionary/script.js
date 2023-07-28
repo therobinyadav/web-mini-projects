05c2112b7308f89eaae5e09b60e38fbf87ffcd1c
Robin Yadav <100231104+therobinyadav@users.noreply.github.com>
2023-07-28 20:45:25 +0530

    Update: Render data on page and style it

--- english-dictionary/script.js
+++ english-dictionary/script.js
@@ -2,6 +2,7 @@
 
 const wordInputEl = document.querySelector('.word-input');
 const searchBtnEl = document.querySelector('.btn-search');
+const resultBoxEl = document.querySelector('.result-box');
 
 const getMeaning = async function (word) {
   const res = await fetch(
@@ -9,11 +10,27 @@ const getMeaning = async function (word) {
   );
   const [data] = await res.json();
   const result = {
-    word: data.word,
+    word: data.word.replace(data.word[0], data.word[0].toUpperCase()),
     meaning: data.meanings[0].definitions[0].definition,
     audioSrc: data.phonetics[0].audio,
   };
-  console.log(result);
+  renderResult(result);
+};
+
+const renderResult = function (resultObj) {
+  resultBoxEl.innerHTML = '';
+  const markup = `
+    <div class="result">
+      <h2 class="word">${resultObj.word}</h2>
+      <p class="meaning">${resultObj.meaning}</p>
+      <audio
+      controls
+      class="word-audio"
+      src="${resultObj.audioSrc}">
+      </audio>
+    </div>
+  `;
+  resultBoxEl.insertAdjacentHTML('beforeend', markup);
 };
 
 searchBtnEl.addEventListener('click', function (e) {
