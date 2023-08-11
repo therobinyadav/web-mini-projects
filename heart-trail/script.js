5c772ee5e0963f74d2d63aa40b1fe93ee4117d7b
Robin Yadav <100231104+therobinyadav@users.noreply.github.com>
2023-08-11 19:14:17 +0530

    Update: Handle moving touch event on mobile
    Do the same thing in mobile that happens on moving mouse on desktop.

--- heart-trail/script.js
+++ heart-trail/script.js
@@ -2,20 +2,22 @@
 
 const bodyEl = document.querySelector('body');
 
-bodyEl.addEventListener('mousemove', function (e) {
-  const heartEL = document.createElement('span');
-  heartEL.classList.add('heart');
+['mousemove', 'touchmove'].forEach(eventType => {
+  bodyEl.addEventListener(eventType, function (e) {
+    const heartEL = document.createElement('span');
+    heartEL.classList.add('heart');
 
-  heartEL.style.left = `${e.offsetX}px`;
-  heartEL.style.top = `${e.offsetY}px`;
+    heartEL.style.left = `${e.offsetX || e.touches[0].clientX}px`;
+    heartEL.style.top = `${e.offsetY || e.touches[0].clientY}px`;
 
-  const size = `${Math.floor(Math.random() * 81) + 20}px`;
-  heartEL.style.width = size;
-  heartEL.style.height = size;
+    const size = `${Math.floor(Math.random() * 81) + 20}px`;
+    heartEL.style.width = size;
+    heartEL.style.height = size;
 
-  bodyEl.appendChild(heartEL);
+    bodyEl.appendChild(heartEL);
 
-  setTimeout(() => {
-    heartEL.remove();
-  }, 3000);
+    setTimeout(() => {
+      heartEL.remove();
+    }, 3000);
+  });
 });
